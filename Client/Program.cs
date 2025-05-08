5b491065549fd5143c7f791df871bccd0c35f05c
Jerry Nixon <jerry.nixon@microsoft.com>
2025-05-07 20:56:58 -0600

    Renamed to Function

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,20 +2,7 @@ using Client.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// read from environment or configuration
-var endpoint = builder.Configuration["AZURE_OPENAI_ENDPOINT"];
-var key = builder.Configuration["AZURE_OPENAI_KEY"];
-
-builder.Services.AddHttpClient("AzureOpenAI", client =>
-{
-    client.BaseAddress = new Uri(endpoint!);
-    client.DefaultRequestHeaders.Add("api-key", key!); // Azure OpenAI expects this header
-});
-
-// optional: inject typed HttpClient if desired
-builder.Services.AddScoped(sp =>
-    sp.GetRequiredService<IHttpClientFactory>().CreateClient("AzureOpenAI"));
-
+// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -25,10 +12,13 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+
 app.UseAntiforgery();
 
 app.MapStaticAssets();
